539799ca290b901636f52060a7f3078f08818ae8
Шакирова Лилия Денисовна <lideshakirova@edu.hse.ru>
2026-05-31 13:57:21 +0300

    добавлены тесты для класса Recipe с фикстурой strawberry_milkshake

--- test_recipes.py
+++ test_recipes.py
@@ -23,3 +23,43 @@ def test_ing_eq(strawberry):
 def test_ing_quantity(strawberry):
     with pytest.raises(ValueError):
         Ingredient("Клубника", -50, "г")
+
+@pytest.fixture
+def strawberry_milkshake():
+    ingredients = [
+        Ingredient("Клубника", 200, "г"),
+        Ingredient("Молоко", 300, "мл"),
+        Ingredient("Сахар", 50, "г")
+    ]
+    return Recipe("Клубничный милкшейк", ingredients)
+
+def test_recipe_add_new(strawberry_milkshake):
+    new_ing = Ingredient("Мороженое", 100, "г")
+    strawberry_milkshake.add_ingredient(new_ing)
+    
+    assert len(strawberry_milkshake.ingredients) == 4
+    assert new_ing in strawberry_milkshake.ingredients
+
+def test_recipe_add_exist(strawberry_milkshake):
+    new_ing = Ingredient("Клубника", 100, "г")
+    strawberry_milkshake.add_ingredient(new_ing)
+    
+    assert len(strawberry_milkshake.ingredients) == 3
+    for ing in strawberry_milkshake.ingredients:
+        if ing.name == "Клубника":
+            assert ing.quantity == 300.0
+
+def test_recipe_scale(strawberry_milkshake):
+    scaled = strawberry_milkshake.scale(2)
+    assert len(scaled.ingredients) == 3
+    for ing in scaled.ingredients:
+        if ing.name == "Клубника":
+            assert ing.quantity == 400.0
+        elif ing.name == "Молоко":
+            assert ing.quantity == 600.0
+        elif ing.name == "Сахар":
+            assert ing.quantity == 100.0
+
+def test_recipe_scale_error(strawberry_milkshake):
+    with pytest.raises(ValueError):
+        strawberry_milkshake.scale(-1)
