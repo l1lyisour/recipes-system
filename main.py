90c75dc8deaf55129c4e46e12bb12cbb7f626e42
Шакирова Лилия Денисовна <lideshakirova@edu.hse.ru>
2026-05-31 13:25:29 +0300

    реализован класс Recipe

--- main.py
+++ main.py
@@ -28,3 +28,37 @@ class Ingredient:
         if not isinstance(other,Ingredient):
             return False
         return self.name == other.name and self.unit == other.unit
+
+class Recipe:
+    def __init__(self,title,ingredients):
+        self.title = title
+        self.ingredients = ingredients if ingredients else []
+    
+    def add_ingredient(self,ingredient: Ingredient):
+        for item in self.ingredients:
+            if item == ingredient:
+                item.quantity += ingredient.quantity
+                return 
+                
+        self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        return isinstance(ratio,(int,float)) and ratio > 0
+
+    def scale(self,ratio):
+        if not self.is_valid_ratio(ratio):
+            raise ValueError('Коэффициент должен быть положительным')
+        new_ingredients = []
+        for i in self.ingredients:
+            new_ing = Ingredient(i.name,i.quantity*ratio,i.unit)
+            new_ingredients.append(new_ing)
+        
+        return Recipe(self.title,new_ingredients)
+
+    def __len__(self):
+        return len(self.ingredients)
+    
+    def __str__(self):
+        ingredients_line = ', '.join(str(i) for i in self.ingredients)
+        return f'{self.title} : {ingredients_line}'
