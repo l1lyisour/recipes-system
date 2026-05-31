d3470c91ab35e9b290c7f6d41cd8b0141b610ce9
Шакирова Лилия Денисовна <lideshakirova@edu.hse.ru>
2026-05-31 13:31:19 +0300

    реализован класс DietaryRecipe, наследующийся от Recipe

--- main.py
+++ main.py
@@ -95,3 +95,17 @@ class ShoppingList:
         new_list._items = self._items + other._items
         
         return new_list
+
+class DietaryRecipe(Recipe):
+    def __init__(self,title,diet_type,ingredients):
+        super().__init__(title,ingredients)
+        self.diet_type = diet_type
+    
+    def scale(self, ratio):
+        if not self.is_valid_ratio(ratio):
+            raise ValueError('Коэффициент должен быть положительным')
+        scaled = super().scale(ratio)
+        return DietaryRecipe(self.title, self.diet_type,scaled.ingredients)
+    
+    def __str__(self):
+        return f'[{self.diet_type}] {super().__str__()}'
